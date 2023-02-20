00a277f290e4171830e1d21c76b0a7f8bda03723
Daniel Augustynowicz <avgustinovich4@gmail.com>
2023-02-20 22:36:58 +0100

    1480. Running Sum of 1d Array - solved

---
+++ 1480. Running Sum of 1D Array.playground/Contents.swift
@@ -0,0 +1,48 @@
+/*
+1480. Running Sum of 1d Array
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+*/
+
+import UIKit
+
+class Solution {
+    //first solution
+    //time complexity = O(n)
+    func runningSum(_ nums: [Int]) -> [Int] {
+        // I create new array
+        var resultArray: [Int] = []
+        // I append to new array first element in given array
+        resultArray.append(nums[0])
+        // I pass the loop starting from the second element in array(with index 1)
+        for i in 1..<nums.count {
+            resultArray.append(nums[i] + resultArray[i - 1])
+        }
+        return resultArray
+    }
+}
+
+var classSol = Solution()
+classSol.runningSum([1,2,3,4,5])
