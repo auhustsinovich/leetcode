af32b38db879fc013352c38d766153fbcd081ae4
Daniel Augustynowicz <avgustinovich4@gmail.com>
2023-03-01 16:18:01 +0100

    1528. Shuffle String - approach with 2 arrays

---
+++ 1528. Shuffle String.playground/Contents.swift
@@ -0,0 +1,41 @@
+/*
+ 1528. Shuffle String
+ You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string. Return the shuffled string.
+
+ Example 1:
+
+ Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+ Output: "leetcode"
+ Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+ Example 2:
+
+ Input: s = "abc", indices = [0,1,2]
+ Output: "abc"
+ Explanation: After shuffling, each character remains in its position.
+
+ Constraints:
+
+ s.length == indices.length == n
+ 1 <= n <= 100
+ s consists of only lowercase English letters.
+ 0 <= indices[i] < n
+ All values of indices are unique.
+ */
+
+// Approach with creating 2 arrays
+// Time complexity - O(N)
+class Solution {
+    func restoreString(_ s: String, _ indices: [Int]) -> String {
+        let copiedArray = Array(s)
+        var resultArray = Array<Character>(repeating: "a", count: indices.count)
+        for i in 0..<indices.count {
+            resultArray[indices[i]] = copiedArray[i]
+        }
+        return String(resultArray)
+    }
+}
+
+var sol = Solution()
+sol.restoreString("codeleet", [4,5,6,7,0,2,1,3])
+
