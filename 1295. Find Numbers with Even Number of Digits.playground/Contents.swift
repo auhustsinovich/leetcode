a9b8221cfc187bc191824a7c2655924ecaf83d97
Daniel Augustynowicz <avgustinovich4@gmail.com>
2023-03-11 22:35:18 +0100

    Straight solution with creating an array of integers

---
+++ 1295. Find Numbers with Even Number of Digits.playground/Contents.swift
@@ -0,0 +1,52 @@
+/*
+ 1295. Find Numbers with Even Number of Digits
+
+ Given an array nums of integers, return how many of them contain an even number of digits.
+
+ Example 1:
+
+ Input: nums = [12,345,2,6,7896]
+ Output: 2
+ Explanation:
+ 12 contains 2 digits (even number of digits).
+ 345 contains 3 digits (odd number of digits).
+ 2 contains 1 digit (odd number of digits).
+ 6 contains 1 digit (odd number of digits).
+ 7896 contains 4 digits (even number of digits).
+ Therefore only 12 and 7896 contain an even number of digits.
+ Example 2:
+
+ Input: nums = [555,901,482,1771]
+ Output: 1
+ Explanation:
+ Only 1771 contains an even number of digits.
+
+
+ Constraints:
+
+ 1 <= nums.length <= 500
+ 1 <= nums[i] <= 105
+ */
+
+class Solution {
+    // Straight solution with creating an array of integers
+    // Time compelxity O(N)
+    func findNumbers(_ nums: [Int]) -> Int {
+        var stringNums: [String] = []
+        var counter: Int = 0
+        // here we append string elements to our array
+        for num in nums {
+            stringNums.append(String(num))
+        }
+        // here we update the counter
+        for stringNum in stringNums {
+            if stringNum.count % 2 == 0 {
+                counter += 1
+            }
+        }
+        return counter
+    }
+}
+
+var sol = Solution()
+sol.findNumbers([142,14,54354,7324,83,10])
